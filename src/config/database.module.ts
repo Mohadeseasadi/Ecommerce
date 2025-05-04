a55ddf9a426717e5e7f4278c61cd86866ee26160
mohadese <mohiw.1380@gmail.com>
2025-05-04 23:54:03 +0330

    feat
    connect to postgres

---
+++ src/config/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}) , 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    
+  ],
+})
+export class DatabaseModule {}
