a37354e485ad8b52f55814f01a2958dcce9bf970
mohadese <mohiw.1380@gmail.com>
2025-05-04 23:58:59 +0330

    feat
    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './config/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    DatabaseModule ,
+    DatabaseModule,
+    UsersModule ,
   ],
   controllers: [],
   providers: [],
