498884d88d5b9602b6f12269d459dec8f25b311a
mohadese <mohiw.1380@gmail.com>
2025-05-05 21:45:37 +0330

    feat
    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import UserRoleEnum from "../enums/user.enum";
+
+@Entity({name:'users'})
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number ; 
+
+    @Column({ unique: true })
+    phone: string ;
+
+    @Column({ nullable: false })
+    display_name: string ;
+    
+    @Column({ nullable: true })
+    password: string ;
+
+    @Column({ type:'enum' , enum: UserRoleEnum, default:UserRoleEnum.User })
+    role: UserRoleEnum ;
+
+    @CreateDateColumn()
+    createAt: Date ;
+
+    @UpdateDateColumn()
+    updateAt: Date ;
+    
+}
