4be1053b24746750db868e4cb931e1d74746f13a
mohadese <mohiw.1380@gmail.com>
2025-05-05 22:12:55 +0330

    feat
    set validation and dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEnum, IsNotEmpty, IsOptional, IsString, Matches, MinLength } from "class-validator";
+import UserRoleEnum from "../enums/user.enum";
+import { Transform } from "class-transformer";
+
+export class CreateUserDto {
+
+    @IsString()
+    @MinLength(2)
+    @IsNotEmpty()
+    display_name: string;
+
+    @IsString()
+    @Matches(/^.{11}$/ , {message: "invalid phone number"})
+    @Transform(({value}) =>  value.trim() )
+    @IsNotEmpty()
+    phone: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(8, {message: "password length is 8 charcter"})
+    password: string;
+    
+    @IsEnum(UserRoleEnum, {message:"role is required"})
+    @IsOptional()
+    role: UserRoleEnum;
+}
