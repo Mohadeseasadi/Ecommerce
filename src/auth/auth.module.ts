550876c18aa3444a014eba44d162053d91ddb6da
mohadese <mohiw.1380@gmail.com>
2025-05-11 19:18:29 +0330

    fix
    completed auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  providers: [AuthService] , 
+  providers: [AuthService , JwtStrategy] , 
   imports: [
+    PassportModule ,
     UsersModule ,     
-    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1h' }}),
+    JwtModule.registerAsync({ 
+      imports:[ConfigModule] , 
+      inject: [ConfigService] ,
+      useFactory: async( configService: ConfigService ): Promise<{}> => ({
+        secret: configService.get('JWT_SECRET') ,
+        signOption: { expireIn: configService.get('JWT_EXPIRATION') }
+      })
+    }),
   ]
 })
 export class AuthModule {}
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,13 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) { 
 
-    constructor(){
+    constructor(configService: ConfigService){
         super({
-            jwtFromRequest: ExteractJwt
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
+            ignoreExpiiration: false ,
+            secretOrKey: configService.get('JWT_SECRET')
         }) 
+    }
+
+    async validate(payload : any) {
+        return {
+            userId: payload.sub , 
+            phone: payload.phone , 
+            display_name: payload.display_name 
+        }
     }
 }
