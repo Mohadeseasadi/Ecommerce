7c95b2be87820db08455d636fc96748a4f836ee4
mohadese <mohiw.1380@gmail.com>
2025-05-14 22:40:16 +0330

    feat
    create address module folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './config/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
     DatabaseModule,
     UsersModule,
-    AuthModule ,
+    AuthModule,
+    AddressModule ,
   ],
   controllers: [],
   providers: [],
