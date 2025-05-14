e0ba210e80ac7ffd2633d2ca9aba91e88ed96d09
mohadese <mohiw.1380@gmail.com>
2025-05-14 22:54:41 +0330

    feat
    create address entity

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,1 +1,38 @@
-export class Address {}
+import { User } from "src/users/entities/user.entity";
+import { Collection, Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity({name: 'addresses'})
+export class Address {
+    
+    @PrimaryGeneratedColumn()
+    id: number ; 
+
+    @Column({ nullable: false })
+    privince: string ;
+
+    @Column({ nullable: false })
+    city: string;
+
+
+    @Column({ nullable: false })
+    address: string;
+
+    @Column({ length: 10 })
+    postal_code: string;
+
+    @Column({ length: 11 })
+    reciver_mobile: string;
+
+    @Column({ nullable: true })
+    description: string;
+
+    @ManyToOne(()=> User , (user: User): any=> user.addresses)
+    user: User;
+
+    @CreateDateColumn()
+    create_at: Date;
+
+    @UpdateDateColumn()
+    update_at: Date;
+}
