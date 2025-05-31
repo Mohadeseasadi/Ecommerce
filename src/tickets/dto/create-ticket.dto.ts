f8e69467134cb799c73bc3495f3b0f513b20034b
mohadese <mohiw.1380@gmail.com>
2025-05-31 18:32:01 +0330

    feat
    create dto fot ticket

--- src/tickets/dto/create-ticket.dto.ts
+++ src/tickets/dto/create-ticket.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTicketDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateTicketDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  subject: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsNumber()
+  @IsOptional()
+  replyTo: number;
+}
