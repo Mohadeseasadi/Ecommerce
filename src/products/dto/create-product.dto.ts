df85e6b1f674803c5b60d0e8eb6cca8704050f3c
mohadese <mohiw.1380@gmail.com>
2025-07-09 23:27:17 +0330

    faet
    create product and category dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,20 @@
-export class CreateProductDto {}
+import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  title: string;
+
+  @IsInt()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsInt()
+  stock: number;
+
+  @IsOptional()
+  @IsArray()
+  categoryIds?: number[];
+}
