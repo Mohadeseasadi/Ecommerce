1eee4ea2be8888a2af5769ee6f656020c92b71dc
mohadese <mohiw.1380@gmail.com>
2025-07-09 23:57:35 +0330

    fix
    import product + category repo in product service and module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/category/entities/category.entity';
+import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Category])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/category/entities/category.entity';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
