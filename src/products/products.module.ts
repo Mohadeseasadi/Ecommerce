e876c333f7a162ba2e6e77bb36c5e3724c80ae34
mohadese <mohiw.1380@gmail.com>
2025-07-16 23:07:03 +0330

    feat
    added create bookmark service

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from 'src/category/entities/category.entity';
+import { UsersModule } from 'src/users/users.module';
+import { Bookmark } from './entities/product-bookmark.entity';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { BookmarksService } from './services/bookmark.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Category])],
+  imports: [
+    TypeOrmModule.forFeature([Product, Category, Bookmark]),
+    UsersModule,
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, BookmarksService],
 })
 export class ProductsModule {}
---
+++ src/products/services/bookmark.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { BookmarkProductDTO } from '../dto/create-bookmark.dto';
+import { Bookmark } from '../entities/product-bookmark.entity';
+import { ProductsService } from '../products.service';
+
+@Injectable()
+export class BookmarksService {
+  constructor(
+    @InjectRepository(Bookmark)
+    private readonly bookmarkRepo: Repository<Bookmark>,
+
+    private readonly productService: ProductsService,
+    private readonly userService: UsersService,
+  ) {}
+
+  async create(
+    createBookmarkDto: BookmarkProductDTO,
+  ): Promise<{ bookmark: Bookmark; msg: string }> {
+    const user = await this.userService.findOne(createBookmarkDto.user_id);
+    const product = await this.productService.findOne(
+      createBookmarkDto.product_id,
+    );
+
+    const exitingBookmark = await this.bookmarkRepo.findOne({
+      where: { user: user, product: product },
+    });
+
+    if (exitingBookmark) {
+      const bookmark = await this.bookmarkRepo.remove(exitingBookmark);
+      return { bookmark, msg: 'deleted bookmark' };
+    } else {
+      const newBookmark = this.bookmarkRepo.create({
+        user,
+        product,
+      });
+      const bookmark = await this.bookmarkRepo.save(newBookmark);
+      return { bookmark, msg: 'created bookmark' };
+    }
+  }
+
+  async findAll() {}
+}
