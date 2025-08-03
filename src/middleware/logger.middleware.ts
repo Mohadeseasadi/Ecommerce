1a01a5d7bd1307bf436a77dac5340e15d62fa6ea
mohadese <mohiw.1380@gmail.com>
2025-08-03 21:47:31 +0330

    faet
    write logger middleware

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(
+        `${req.method} | ${req.url} | ${res.status} | ${duration} ms`,
+      );
+    });
+    next();
+  }
+}
