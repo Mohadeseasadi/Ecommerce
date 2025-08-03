0af3cbfb5d8afa70946447a0c354879886f4c578
mohadese <mohiw.1380@gmail.com>
2025-08-03 22:01:33 +0330

    feat
    add middleware to address module for some route

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 import { User } from 'src/users/entities/user.entity';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
@@ -11,4 +12,13 @@ import { Address } from './entities/address.entity';
   providers: [AddressService],
   exports: [AddressService],
 })
-export class AddressModule {}
+export class AddressModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        { path: 'address', method: RequestMethod.POST },
+        { path: 'address/:id', method: RequestMethod.GET },
+      );
+  }
+}
