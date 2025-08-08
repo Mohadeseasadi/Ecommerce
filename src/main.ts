a4cc0b0d97fbc6a0cf467cfe119cee7578af3605
mohadese <mohiw.1380@gmail.com>
2025-08-08 16:01:25 +0330

    faet
    install and setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
@@ -8,6 +9,17 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(new LoggerMiddleware().use);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommers - Rest API')
+    .setDescription('')
+    .setVersion('1')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(process.env.PORT ?? 3000, () =>
     console.log(`run project on port : ${process.env.PORT ?? 3000}`),
