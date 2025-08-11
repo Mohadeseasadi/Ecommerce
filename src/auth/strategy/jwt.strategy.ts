23724a7a16af10cce6bb924aa4bc7520aab12a33
mohadese <mohiw.1380@gmail.com>
2025-08-11 23:01:28 +0330

    faet
    added guard to project

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,24 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) { 
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
 
-    constructor(configService: ConfigService){
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
-            ignoreExpiiration: false ,
-            secretOrKey: configService.get('JWT_SECRET')
-        }) 
-    }
-
-    async validate(payload : any) {
-        return {
-            userId: payload.sub , 
-            phone: payload.phone , 
-            display_name: payload.display_name 
-        }
-    }
+  async validate(payload: any) {
+    return {
+      userId: payload.sub,
+      phone: payload.phone,
+      display_name: payload.display_name,
+    };
+  }
 }
